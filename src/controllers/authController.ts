321ff09c8730aaeadfcfb4c1338432afdd372bce
farah707 <farah.tarek@msa.edu.eg>
2026-07-25 19:04:56 +0300

    Fix authentication controller and login/register handling

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,59 +1,88 @@
 import bcrypt from "bcrypt";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import User from "../models/User";
 import { generateToken } from "../utils/jwt";
 
-export async function register(req: Request, res: Response) {
-const hashedPassword = await bcrypt.hash(req.body.password, 10);
+export const register = async (req: Request, res: Response, next: NextFunction) => {
+try {
+    const { name, email, password } = req.body;
 
-const user = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: hashedPassword,
-});
+    const existingUser = await User.findOne({ email });
 
-const token = generateToken(user._id.toString());
-return res.status(201).json({
-    token,
-    user: {
-    id: user._id,
-    name: user.name,
-    email: user.email,
-    },
-});
-}
-
-export async function login(req: Request, res: Response) {
-const user = await User.findOne({
-    email: req.body.email,
-});
-
-if (!user) {
-    return res.status(401).json({
-    message: "Invalid credentials",
+    if (existingUser) {
+    return res.status(400).json({
+        error: {
+        message: "Email already exists"
+        }
     });
-}
+    }
 
-const valid = await bcrypt.compare(
-    req.body.password,
-    user.password
-);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-if (!valid) {
-    return res.status(401).json({
-    message: "Invalid credentials",
+    const user = await User.create({
+    name,
+    email,
+    password: hashedPassword
     });
-}
 
-const token = generateToken(user._id.toString());
-
-return res.status(200).json({
-    token,
+    res.status(201).json({
+    message: "User registered successfully",
     user: {
-    id: user._id,
-    name: user.name,
-    email: user.email,
-    },
-});
-}
+        id: user._id,
+        name: user.name,
+        email: user.email
+    }
+    });
 
+} catch (error) {
+    next(error);
+}
+};
+export async function login(
+    req: Request,
+    res: Response,
+    next: NextFunction
+    ) {
+    try {
+        const user = await User.findOne({
+        email: req.body.email,
+        });
+
+        if (!user) {
+        return res.status(401).json({
+            error: {
+            message: "Invalid credentials",
+            },
+        });
+        }
+
+        const valid = await bcrypt.compare(
+        req.body.password,
+        user.password
+        );
+
+        if (!valid) {
+        return res.status(401).json({
+            error: {
+            message: "Invalid credentials",
+            },
+        });
+        }
+
+        const token = generateToken(user._id.toString());
+
+        return res.status(200).json({
+        message: "Login successful",
+        token,
+        user: {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+        },
+        });
+
+    } catch (error) {
+        next(error);
+    
+}
+    }
